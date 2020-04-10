080b62c2493f85124c56fe4205512b0ba95ba44c
fomin_k <fomin_k@npcprom.ru>
2020-04-10 15:56:59 +0500

    добавил файл

---
+++ C++/alsenconst.h
@@ -0,0 +1,12 @@
+#ifndef ALSENCONST_H
+#define ALSENCONST_H
+
+#include <QtGlobal>
+
+// значение двубита для разных стартовых углов
+const quint8 VAL_FI_0     = 0;
+const quint8 VAL_FI_90    = 1;
+const quint8 VAL_FI_270   = 2;
+const quint8 VAL_FI_180   = 3;
+
+#endif // ALSENCONST_H
